815c93d568fc68146b0b34dbaafc349d04bd54d9
Macktireh <abdimack97@gmail.com>
2024-10-14 11:50:56 +0200

    create my website with astro

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	vite: {
+		server: {
+			watch: {
+				usePolling: true,
+			}
+		}
+	},
+	integrations: [mdx(), sitemap()],
+	i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es", "fr"],
+  },
+});
