b81905523f0079c7cd8be5c2bbc442c9527800f8
Macktireh <abdimack97@gmail.com>
2024-10-20 01:16:42 +0200

    added astro-expressive-code with dracula theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			}
-		}
-	},
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+  integrations: [
+    expressiveCode({
+      // Example: Change the theme to "dracula"
+      theme: "dracula",
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
