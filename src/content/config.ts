71be62c5576b27b07445e3feda2fab7180c0d79a
Macktireh <abdimack97@gmail.com>
2024-10-26 18:38:58 +0200

    code refactoring

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-import type { LangType } from "@src/i18n/utils";
 import { defineCollection, z, type CollectionEntry } from "astro:content";
 import { getCollection } from "astro:content";
+import { LOCALES, type LangType } from "@src/i18n/utils";
 
 const blog = defineCollection({
   // Type-check frontmatter using a schema
@@ -11,31 +11,36 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
-    lang: z.string().optional(),
+    lang: z.enum(LOCALES as [string, ...string[]]),
     author: z.string().optional(),
   }),
 });
 
 export const collections = { blog };
 
-export type BlogType = CollectionEntry<"blog"> & { blog_slug: string };
+export type BlogType = CollectionEntry<"blog"> & {
+  blog_slug: string;
+  pathname: string;
+};
 
-export async function getBlogPosts() {
+export async function getPosts(): Promise<BlogType[]> {
   const posts = await getCollection("blog");
 
-  return posts.map((post) => {
-    const blog_slug = post.slug.split("/")[0];
-    return {
-      ...post,
-      blog_slug,
-    };
-  });
+  return posts
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .map((post) => {
+      const blog_slug = post.slug.split("/")[0];
+      const pathname = `/blog/${blog_slug}`;
+      return {
+        ...post,
+        blog_slug,
+        pathname,
+      };
+    });
 }
 
-export async function getBlogPostsByLang(lang: LangType) {
-  return (await getCollection("blog"))
-    .filter((blog) => {
-      return blog?.data?.lang === lang;
-    })
-    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+export async function getPostsByLang(lang: LangType): Promise<BlogType[]> {
+  return (await getPosts()).filter((blog) => {
+    return blog?.data?.lang === lang;
+  });
 }
