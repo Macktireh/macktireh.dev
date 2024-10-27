b03a4d9c105141e1c79e76b52bea80f8862df445
Macktireh <abdimack97@gmail.com>
2024-10-27 23:11:37 +0100

    Set the 'PUBLIC_SITE_URL' environment variable.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
 import { pluginLanguageBadge } from "./plugins/expressiveCode";
+import { loadEnv } from "vite";
+
+const { PUBLIC_SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+if (!PUBLIC_SITE_URL) {
+  throw new Error("PUBLIC_SITE_URL is not set in your environment variables file '.env'");
+}
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: PUBLIC_SITE_URL,
   vite: {
     server: {
       watch: {
