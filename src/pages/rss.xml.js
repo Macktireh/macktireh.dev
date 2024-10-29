95c657a2b84b55136b2938a4bfdfb4341fc6e5b3
Macktireh <abdimack97@gmail.com>
2024-10-29 14:25:09 +0100

    added rssxml.js for default language

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,18 @@
 import rss from "@astrojs/rss";
 import { getPostsByLang } from "@src/services/posts";
-import { LOCALES, useTranslations } from "@src/i18n/utils";
+import { DEFAULT_LANG, useTranslations } from "@src/i18n/utils";
 
-export function getStaticPaths() {
-  const paths = LOCALES.map((lang) => ({
-    params: { lang },
-  }));
-  return paths;
-}
 
 export async function GET(context) {
-  const { lang } = context.params;
-  const t = useTranslations(lang);
-  const posts = await getPostsByLang(lang);
+  const t = useTranslations(DEFAULT_LANG)
+  const posts = await getPostsByLang(DEFAULT_LANG);
   return rss({
     title: t("site.title"),
     description: t("site.description", { firstname: SITE.authorFirstName }),
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/${lang}${post.pathname}`,
+      link: `/${DEFAULT_LANG}${post.pathname}`,
     })),
   });
 }
