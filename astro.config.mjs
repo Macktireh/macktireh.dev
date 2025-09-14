dbab78c7c4391ce679f1f39f9a6a97ebe0c565ae
Macktireh <abdimack97@gmail.com>
2025-09-15 01:33:28 +0200

    fix: include NODE_ENV in environment variable loading for server configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { pluginLanguageBadge } from "./plugins/expressiveCode";
 import { loadEnv } from "vite";
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
-const { PUBLIC_SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { NODE_ENV, PUBLIC_SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 if (!PUBLIC_SITE_URL) {
   throw new Error("PUBLIC_SITE_URL is not set in your environment variables file '.env'");
@@ -15,6 +15,9 @@ if (!PUBLIC_SITE_URL) {
 // https://astro.build/config
 export default defineConfig({
   site: PUBLIC_SITE_URL,
+  server: {
+    allowedHosts: NODE_ENV === "development" ? true : [],
+  },
   vite: {
     server: {
       watch: {
